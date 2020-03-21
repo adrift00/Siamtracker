ab2b4b6805b3a20caca89910644db11a74c35eb6
adrift <1219660880@qq.com>
2020-03-21 10:32:22 +0800

    don't update the rpn branch and use each examplar for train and test

--- models/grad_siam_model.py
+++ models/grad_siam_model.py
@@ -31,7 +31,7 @@ class GradSiamModel(BaseSiamModel):
         if cfg.ADJUST.USE:
             freeze_module(self.neck)
         # don't freeze the rpn, finetune it
-        # freeze_module(self.rpn)
+        freeze_module(self.rpn)
 
     def forward(self,examplar,
                 train_search, train_gt_cls, train_gt_loc, train_gt_loc_weight,
@@ -43,61 +43,47 @@ class GradSiamModel(BaseSiamModel):
             examplar = self.neck(examplar)
             search = self.neck(search)
             test_search=self.neck(test_search)
-        # # use the first examplar to update the frame
-        # examplar0 = examplar[0, :, :, :][None, :, :, :]
-        # # examplar0.requires_grad_(True)
-        # examplar0.requires_grad_(True)
-        # init_cls_losses = torch.zeros([examplar.size(0)]).cuda()
-        # init_loc_losses = torch.zeros([examplar.size(0)]).cuda()
-        # init_total_losses = torch.zeros([examplar.size(0)]).cuda()
-        # for i in range(examplar.size(0)):
-        #     pred_cls, pred_loc = self.rpn(examplar0, search[i][None, :, :, :])
-        #     pred_cls = self.log_softmax(pred_cls)
-        #     cls_loss = select_cross_entropy_loss(pred_cls, gt_cls[i][None, :, :, :])
-        #     loc_loss = weight_l1_loss(pred_loc, gt_loc[i][None, :, :, :], gt_loc_weight[i][None, :, :, :])
-        #     total_loss = cfg.TRAIN.CLS_WEIGHT * cls_loss + cfg.TRAIN.LOC_WEIGHT * loc_loss
-        #     init_cls_losses[i], init_loc_losses[i], init_total_losses[i] = cls_loss, loc_loss, total_loss
-        # # backward for the grad
-        # init_cls_losses, init_loc_losses, init_total_losses = init_cls_losses.mean(), init_loc_losses.mean(), init_total_losses.mean()
-        # examplar_grad = torch.autograd.grad(init_total_losses, examplar0)[0] * 1000
-        # examplar0 = examplar0 + self.grad_layer(examplar_grad)
-        # # use the new examplar to get the final loss
-        # cls_losses = torch.zeros([examplar.size(0)]).cuda()
-        # loc_losses = torch.zeros([examplar.size(0)]).cuda()
-        # total_losses = torch.zeros([examplar.size(0)]).cuda()
-        # for i in range(examplar.size(0)):
-        #     pred_cls, pred_loc = self.rpn(examplar0, search[i][None, :, :, :])
-        #     pred_cls = self.log_softmax(pred_cls)
-        #     cls_loss = select_cross_entropy_loss(pred_cls, gt_cls[i][None, :, :, :])
-        #     loc_loss = weight_l1_loss(pred_loc, gt_loc[i][None, :, :, :], gt_loc_weight[i][None, :, :, :])
-        #     total_loss = cfg.TRAIN.CLS_WEIGHT * cls_loss + cfg.TRAIN.LOC_WEIGHT * loc_loss
-        #     cls_losses[i], loc_losses[i], total_losses[i] = cls_loss, loc_loss, total_loss
-        # cls_losses, loc_losses, total_losses = cls_losses.mean(), loc_losses.mean(), total_losses.mean()
+        # examplar.requires_grad_(True)
+        # pred_cls, pred_loc = self.rpn(examplar, search)
+        # pred_cls = self.log_softmax(pred_cls)
+        # init_cls_loss = select_cross_entropy_loss(pred_cls, train_gt_cls)
+        # init_loc_loss = weight_l1_loss(pred_loc, train_gt_loc, train_gt_loc_weight)
+        # init_total_loss = cfg.TRAIN.CLS_WEIGHT * init_cls_loss + cfg.TRAIN.LOC_WEIGHT * init_loc_loss
+        # examplar_grad = torch.autograd.grad(init_total_loss, examplar)[0] * 1000
+        # examplar = examplar + self.grad_layer(examplar_grad)
+        #
+        # # for test search
+        # pred_cls, pred_loc = self.rpn(examplar, test_search)
+        # pred_cls = self.log_softmax(pred_cls)
+        # cls_loss = select_cross_entropy_loss(pred_cls, test_gt_cls)
+        # loc_loss = weight_l1_loss(pred_loc, test_gt_loc, test_gt_loc_weight)
+        # total_loss = cfg.TRAIN.CLS_WEIGHT * cls_loss + cfg.TRAIN.LOC_WEIGHT * loc_loss
+
         # return {
-        #     'cls_loss': cls_losses,
-        #     'loc_loss': loc_losses,
-        #     'total_loss': total_losses,
-        #     'init_cls_loss': init_cls_losses,
-        #     'init_loc_loss': init_loc_losses,
-        #     'init_total_loss': init_total_losses,
+        #     'cls_loss': cls_loss,
+        #     'loc_loss': loc_loss,
+        #     'total_loss': total_loss,
+        #     'init_cls_loss': init_cls_loss,
+        #     'init_loc_loss': init_loc_loss,
+        #     'init_total_loss': init_total_loss,
         #     'examplar_grad': examplar_grad
         # }
-        examplar.requires_grad_(True)
-        pred_cls, pred_loc = self.rpn(examplar, search)
+        examplar0=examplar[0,:,:,:][None,:,:,:]
+        new_examplar=examplar0.repeat(examplar.size(0),1,1,1)
+        new_examplar.requires_grad_(True)
+        pred_cls, pred_loc = self.rpn(new_examplar, search)
         pred_cls = self.log_softmax(pred_cls)
         init_cls_loss = select_cross_entropy_loss(pred_cls, train_gt_cls)
         init_loc_loss = weight_l1_loss(pred_loc, train_gt_loc, train_gt_loc_weight)
         init_total_loss = cfg.TRAIN.CLS_WEIGHT * init_cls_loss + cfg.TRAIN.LOC_WEIGHT * init_loc_loss
-        examplar_grad = torch.autograd.grad(init_total_loss, examplar)[0] * 1000
-        examplar = examplar + self.grad_layer(examplar_grad)
+        examplar_grad = torch.autograd.grad(init_total_loss, new_examplar)[0] * 1000
+        new_examplar=new_examplar+self.grad_layer(examplar_grad)
 
-        # for test search
-        pred_cls, pred_loc = self.rpn(examplar, test_search)
+        pred_cls, pred_loc = self.rpn(new_examplar, test_search)
         pred_cls = self.log_softmax(pred_cls)
         cls_loss = select_cross_entropy_loss(pred_cls, test_gt_cls)
         loc_loss = weight_l1_loss(pred_loc, test_gt_loc, test_gt_loc_weight)
         total_loss = cfg.TRAIN.CLS_WEIGHT * cls_loss + cfg.TRAIN.LOC_WEIGHT * loc_loss
-
         return {
             'cls_loss': cls_loss,
             'loc_loss': loc_loss,
