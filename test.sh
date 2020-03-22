3f53fcd554ebfae778a0d32257eba225a98e0d1d
adrift <1219660880@qq.com>
2020-03-22 18:36:42 +0800

    modify the test.sh

--- test.sh
+++ test.sh
@@ -1,1 +1,1 @@
-python test.py --tracker SiamRPN --dataset VOT2018 --cfg configs/mobilenetv2_pruning.yaml --snapshot ./snapshot/mobilenetv2_sfp/checkpoint_e12.pth
+python test.py --tracker SiamRPN --dataset VOT2018 --cfg configs/mobilenetv2_pruning.yaml --snapshot ./snapshot/mobilenetv2_sfp_0_5/checkpoint_e15.pth
