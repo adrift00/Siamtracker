7ab7f00f8d65af1c5f4ccd24eac4316a5e7e5b88
adrift <1219660880@qq.com>
2020-04-03 22:03:33 +0800

    modify model_convert.py for quantization.

--- models/base_siam_model.py
+++ models/base_siam_model.py
@@ -18,22 +18,22 @@ class BaseSiamModel(nn.Module):
 
         self.rpn = get_rpn_head(cfg.RPN.TYPE, **cfg.RPN.KWARGS)
 
-    def forward(self, examplar, search, gt_cls, gt_loc, gt_loc_weight):
-        examplar = self.backbone(examplar)
-        search = self.backbone(search)
-        if cfg.ADJUST.USE:
-            examplar = self.neck(examplar)
-            search = self.neck(search)
-        pred_cls, pred_loc = self.rpn(examplar, search)
-        pred_cls = self.log_softmax(pred_cls)
-        cls_loss = select_cross_entropy_loss(pred_cls, gt_cls)
-        loc_loss = weight_l1_loss(pred_loc, gt_loc, gt_loc_weight)
-        total_loss = cfg.TRAIN.CLS_WEIGHT * cls_loss + cfg.TRAIN.LOC_WEIGHT * loc_loss
-        return {
-            'cls_loss': cls_loss,
-            'loc_loss': loc_loss,
-            'total_loss': total_loss
-        }
+    # def forward(self, examplar, search, gt_cls, gt_loc, gt_loc_weight):
+    #     examplar = self.backbone(examplar)
+    #     search = self.backbone(search)
+    #     if cfg.ADJUST.USE:
+    #         examplar = self.neck(examplar)
+    #         search = self.neck(search)
+    #     pred_cls, pred_loc = self.rpn(examplar, search)
+    #     pred_cls = self.log_softmax(pred_cls)
+    #     cls_loss = select_cross_entropy_loss(pred_cls, gt_cls)
+    #     loc_loss = weight_l1_loss(pred_loc, gt_loc, gt_loc_weight)
+    #     total_loss = cfg.TRAIN.CLS_WEIGHT * cls_loss + cfg.TRAIN.LOC_WEIGHT * loc_loss
+    #     return {
+    #         'cls_loss': cls_loss,
+    #         'loc_loss': loc_loss,
+    #         'total_loss': total_loss
+    #     }
 
     def track(self, search):
         search = self.backbone(search)
@@ -73,6 +73,17 @@ class BaseSiamModel(nn.Module):
     #         search = self.neck(search)
     #     pred_cls, pred_loc = self.rpn(examplar, search)
     #     return pred_cls, pred_loc
+
+    @torch.no_grad()
+    def forward(self,examplar,search):
+        examplar = self.backbone(examplar)
+        search = self.backbone(search)
+        if cfg.ADJUST.USE:
+            examplar = self.neck(examplar)
+            search = self.neck(search)
+        pred_cls, pred_loc = self.rpn(examplar, search)
+        return pred_cls,pred_loc
+
 
     def log_softmax(self, cls):
         b, a2, h, w = cls.size()
--- tools/model_conveter.py
+++ tools/model_conveter.py
@@ -5,6 +5,24 @@ from configs.config import cfg
 import os
 os.environ["CUDA_VISIBLE_DEVICES"] = "2"
 if __name__ == '__main__':
+    # cfg.merge_from_file('configs/mobilenetv2_finetune.yaml')
+    # pretrained_path = 'pretrained_models/siamrpn_mobi.pth'
+    # model = BaseSiamModel().cuda()
+    # model = load_pretrain(model, pretrained_path)
+    # examplar = torch.randn(1, 3, 127, 127, device='cuda')
+    # search = torch.randn(1, 3, 255, 255, device='cuda')
+    # model.eval()
+    # e0,e1,e2 = model.get_examplar(examplar)
+    # torch.onnx.export(model,
+    #                   (e0,e1,e2, search),
+    #                   "pretrained_models/siamrpn_mobi_search.onnx",
+    #                   verbose=True,
+    #                   input_names=['e0', 'e1','e2','search'],
+    #                   output_names=['cls','loc'])
+    # examplar convert
+    # torch.onnx.export(model,examplar,'pretrained_models/siamrpn_mobi_examplar.onnx',verbose=True,output_names=['e0','e1','e2'])
+
+
     cfg.merge_from_file('configs/mobilenetv2_finetune.yaml')
     pretrained_path = 'pretrained_models/siamrpn_mobi.pth'
     model = BaseSiamModel().cuda()
@@ -12,12 +30,16 @@ if __name__ == '__main__':
     examplar = torch.randn(1, 3, 127, 127, device='cuda')
     search = torch.randn(1, 3, 255, 255, device='cuda')
     model.eval()
-    e0,e1,e2 = model.get_examplar(examplar)
     torch.onnx.export(model,
-                      (e0,e1,e2, search),
-                      "pretrained_models/siamrpn_mobi_search.onnx",
+                      (examplar, search),
+                      "pretrained_models/siamrpn_mobi.onnx",
                       verbose=True,
-                      input_names=['e0', 'e1','e2','search'],
+                      input_names=['examplar','search'],
                       output_names=['cls','loc'])
-    # examplar convert
-    # torch.onnx.export(model,examplar,'pretrained_models/siamrpn_mobi_examplar.onnx',verbose=True,output_names=['e0','e1','e2'])
+
+
+
+
+
+
+
