20fa9999638cc78361c48059939a964785df6c3f
adrift <1219660880@qq.com>
2020-04-03 22:27:11 +0800

    the zero shift is useless, use shift as before.

--- test.sh
+++ test.sh
@@ -1,1 +1,1 @@
-python test.py --tracker GradSiamRPN --dataset GOT-10k --cfg configs/alexnet_grad_config.yaml --snapshot ./snapshot/grad_single_loss_multi_data/checkpoint_e7.pth 
+python test.py --tracker GradSiamRPN --dataset VOT2018 --cfg configs/alexnet_grad_config.yaml --snapshot ./snapshot/grad_single_loss_multi_data/checkpoint_e7.pth 
